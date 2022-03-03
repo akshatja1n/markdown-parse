d07d33d5c09c1acb6ecb603b3200e1e9fd5a298f
akshatja1n <34198249+Akshat30@users.noreply.github.com>
2022-03-03 12:29:49 -0800

    Modified MarkdownParse with version where main can take a directory of files.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,12 +1,36 @@
 
 // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MarkdownParse {
+    public static Map<String, List<String>> getLinks(File dirOrFile) throws IOException {
+        Map<String, List<String>> result = new HashMap<>();
+        if(dirOrFile.isDirectory()) {
+            for(File f: dirOrFile.listFiles()) {
+                result.putAll(getLinks(f));
+            }
+            return result;
+        }
+        else {
+            Path p = dirOrFile.toPath();
+            int lastDot = p.toString().lastIndexOf(".");
+            if(lastDot == -1 || !p.toString().substring(lastDot).equals(".md")) {
+                return result;
+            }
+            ArrayList<String> links = getLinks(Files.readString(p));
+            result.put(dirOrFile.getPath(), links);
+            return result;
+        }
+    }
+
     public static ArrayList<String> getLinks(String markdownString) {
 
         String[] markdownStrings = markdownString.split("\n");
@@ -34,9 +58,15 @@ public class MarkdownParse {
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
-        String contents = Files.readString(fileName);
-        ArrayList<String> links = getLinks(contents);
-        System.out.println(links);
+        File file = new File(args[0]);
+        if (file.isDirectory()) {
+            Map<String, List<String>> links = getLinks(file);
+            System.out.println(links);
+        } else {
+            String contents = Files.readString(fileName);
+            ArrayList<String> links = getLinks(contents);
+            System.out.println(links);
+        }
     }
 }
 
